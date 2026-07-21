095599ed69a788c9496dd9ce93e4f21df6786379
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2026-07-21 11:18:46 +0900

    disable host authorization in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow access to the API from any host
+  config.hosts.clear
 end
